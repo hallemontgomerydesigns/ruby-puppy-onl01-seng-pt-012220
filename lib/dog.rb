aa369730f95eb12dbf5e1d3f1b73f9f7f4eadb35
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-02-23 23:12:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,7 +8,7 @@ class Dog
   def 
     initialize(name)
     @name = name
-    @@all << self
+   save
   end
   
   def self.all
@@ -22,6 +22,11 @@ class Dog
  def self.clear_all
    @@all.clear
  end
+ 
+ def save 
+   @@all << self 
+   
+ end 
  
  
 end
